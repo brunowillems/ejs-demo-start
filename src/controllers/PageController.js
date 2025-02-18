0bf1100884c5f2b6a8b0bedc6ae7e4c32cd69075
brunowillems <bruno.willems@student.arteveldehs.be>
2025-02-18 12:47:14 +0100

    controller changes and partials includes

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,8 +1,15 @@
 // res.render("<NAAM VAN DE VIEW>", {"<DATA NAAR DE VIEW>"});"
+
+import {
+    person,
+    navItems, // url, label 
+} from '../data/data.js';
+
 export const home = (req, res) => {
     res.render('pages/home', {
         title: 'Dinosaurs around the world',
-        content: 'Welcome to the orld of dinosaurs!'
+        content: 'Welcome to the world of dinosaurs!',
+        navItems,
     });
 };
 
@@ -11,15 +18,16 @@ export const about = (req, res) => {
         title: 'About us',
         content: 'We are a group of dinosaur enthusiasts.',
         team: ["T-Rex", "Velociraptor", "Stegosaurus", "Triceratops", "<strong>Spinosaurus</strong>"],
+        navItems,
     });
 }
 
-import { person } from '../data/data.js';
 export const contact = (req, res) => {
     res.render('pages/contact', {
         title: 'Contact',
         content: 'Contact us at 09 233 40 89',
         person,
+        navItems,
     });
 }
 
@@ -28,5 +36,6 @@ export const privacy = (req, res) => {
         title: 'Privacy Policy',
         content: 'Dinosaurs are protective of their privacy.',
         team: false,
+        navItems,
     });
 }
