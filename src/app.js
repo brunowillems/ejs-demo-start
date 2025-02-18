7d5e9f10a08faf293c6d3de1fc7fae49272c0075
brunowillems <bruno.willems@student.arteveldehs.be>
2025-02-18 13:29:09 +0100

    multiple changes on ejs and errors addons

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 // import statements
 import express from "express";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
 
 import { home, about, contact, privacy } from "./controllers/PageController.js";
 
 // create an instance of express
 const app = express();
 
+// use express layout
+app.use(expressLayouts);
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
+app.set("layout", "layouts/main");
 app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
@@ -20,6 +25,12 @@ app.get("/", home);
 app.get("/about", about);
 app.get("/contact", contact);
 app.get("/privacy", privacy);
+
+app.get('*', (req, res) => {
+  res.status(404).render('errors/404', {
+    layout: 'layouts/error',
+  });
+});
 
 // start the server, listen on port defined in .env file
 app.listen(process.env.PORT, () => {
