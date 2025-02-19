9325963140196f354b36d8dcfbfd756ff9263766
brunowillems <bruno.willems@student.arteveldehs.be>
2025-02-19 14:56:40 +0100

    changes on controller and index.ejs

--- src/controllers/DinoController.js
+++ src/controllers/DinoController.js
@@ -1,16 +1,18 @@
+import { dinosaurs } from "../data/data.js";
+
 export const index = (req, res) => {
     res.render('pages/dinosaurs', {
-        title: `${dinosaurs.name}`,
-        herbivore: `${dinosaurs.isHerbivore}`,
-        image: `${dinosaurs.image}`,
+        title: dinosaurs.name,
+        herbivore: dinosaurs.isHerbivore,
+        image: dinosaurs.image,
     });
 }
 
 export const detail = (req, res) => {
     res.render('pages/dinosaur', {
-        title: `${dinosaurs.name}`,
-        herbivore: `${dinosaurs.isHerbivore}`,
-        image: `${dinosaurs.image}`,
-        description: `${dinosaurs.description}`,
+        title: dinosaurs.name,
+        herbivore: dinosaurs.isHerbivore,
+        image: dinosaurs.image,
+        description: dinosaurs.description,
     });
 }
